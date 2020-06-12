1d99fcebd3810aa63d7842b1f2833c23c1bf5a19
Anup <anupgpatil@gmail.com>
2020-06-12 11:27:18 -0700

    Adding A Rubber Duck to the Line

--- src/main/java/com/designpatterns/strategypattern/StrategypatternApplication.java
+++ src/main/java/com/designpatterns/strategypattern/StrategypatternApplication.java
@@ -1,6 +1,8 @@
 package com.designpatterns.strategypattern;
 
 import com.designpatterns.strategypattern.problem.MallardDuck;
+import com.designpatterns.strategypattern.problem.RedDuck;
+import com.designpatterns.strategypattern.problem.RubberDuck;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -15,10 +17,25 @@ public class StrategypatternApplication {
     }
 
     private static void problem(){
+        System.out.println("Mallard Duck Behaviors");
         MallardDuck mallardDuck = new MallardDuck();
         mallardDuck.quack();
         mallardDuck.swim();
         mallardDuck.display();
+
+        System.out.println("Red Duck Behaviors");
+
+        RedDuck redDuck = new RedDuck();
+        redDuck.quack();
+        redDuck.swim();
+        redDuck.display();
+
+        System.out.println("Rubber Duckk Behaviors");
+
+        RubberDuck rubberDuck = new RubberDuck();
+        rubberDuck.display();
+        rubberDuck.quack();
+        rubberDuck.swim();
     }
 
 }
---
+++ src/main/java/com/designpatterns/strategypattern/problem/RubberDuck.java
@@ -0,0 +1,8 @@
+package com.designpatterns.strategypattern.problem;
+
+public class RubberDuck extends  Duck{
+    @Override
+    public void display() {
+        System.out.println("Display -------> I am a Rubber Duck");
+    }
+}
